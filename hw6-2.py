cbd34f5b63d94cfe1b1a5fed2cc62e2e73ae26d3
p22rstewart <p22rstewart@fairfieldprep.org>
2021-11-09 10:20:33 -0500

    hw

---
+++ hw6-2.py
@@ -0,0 +1,31 @@
+# Author RTS 11/9/21
+
+# Question 1
+my_lst = [0, 1, 2, 3, 4, 5, 6, 7]
+
+# Question 2
+print(len(my_lst))
+
+# Question 3
+print(my_lst[3])
+
+# Question 4
+print(5 not in my_lst)
+
+# Question 5
+print(10 not in my_lst)
+
+# Question 6
+my_lst2 = "Chris" + "Nolan" + "Ryan"
+print(list(my_lst2))
+
+# Question 7
+print(my_lst2[10:])
+
+# Question 8
+print(my_lst2[10:] * 3)
+
+# Question 9
+my_2lst = my_lst + my_lst2
+print(my_2lst)
+
